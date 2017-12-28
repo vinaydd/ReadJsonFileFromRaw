98973f789e4f37e56ca968b56f04a48887f72c10
Sharad Singh <sharadsingh@Sharads-Mac-mini-2.local>
2017-12-28 13:12:30 +0530

    Initial commit

---
+++ app/src/main/java/com/truxapp/sharadsingh/myapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.truxapp.sharadsingh.myapplication;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            JSONObject obj = new JSONObject(loadJSONFromAsset(this));
+            String zd = obj.toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String loadJSONFromAsset(Context context) {
+        String json = null;
+        try {
+            String path = "android.resource://" + getPackageName() + "/" + R.raw.gcclist;
+            //  path = "android.resource://" + getPackageName() + "/" + R.raw.gcclist;
+            InputStream is = getResources().openRawResource(R.raw.gcclist);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
+    }
+
+
+}
