586c3e66860f41e025f1ea5a6da10c48e62caf68
vinaysingh <singhvinay0038@gmail.com>
2017-12-28 14:28:44 +0530

    space define AN COMMENTED

--- app/src/main/java/com/truxapp/sharadsingh/myapplication/MainActivity.java
+++ app/src/main/java/com/truxapp/sharadsingh/myapplication/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
             JSONObject obj = new JSONObject(loadJSONFromAsset(this));
 
 //            vinay singh
-
+            ///MEDIA_PROJECTION_SERVICE
 
             // RAHUL SINGHEWREWARFEWFERWF
             String zd = obj.toString();
